ee39a4750fa654667b12809c7ee7cdc213ce9223
GreenCheeseWheel <thegreencheesewheel@gmail.com>
2023-01-14 17:50:52 -0300

    styling update

--- src/data.js
+++ src/data.js
@@ -38,22 +38,22 @@ export default {
     proyectoData: [
         {
             imgData: "./img/proy/CustomerRel.png",
-            textData: "CRM Application",
+            textData: "CRM Application (Back-end)",
             urlData: "https://github.com/GreenCheeseWheel/CustomerRelManag"
         },
         {
             imgData: "./img/proy/ECommerce.png",
-            textData: "E-Commerce",
+            textData: "E-Commerce (Front-end)",
             urlData: "https://github.com/GreenCheeseWheel/ECommerceWeb"
         },
         {
             imgData: "./img/proy/Landing.png",
-            textData: "Landing Page",
+            textData: "Landing Page (Front-end)",
             urlData: "https://github.com/GreenCheeseWheel/LandingPage"
         },
         {
             imgData: "./img/proy/Security.png",
-            textData: "Spring Security Application",
+            textData: "Spring Security Application (Back-end)",
             urlData: "https://github.com/GreenCheeseWheel/secureAppDemo"
         },
 

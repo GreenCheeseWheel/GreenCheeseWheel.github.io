8ca552dcd21b053cdff485109a04363c8afc0680
GreenCheeseWheel <thegreencheesewheel@gmail.com>
2023-01-14 09:14:03 -0300

    Updates

---
+++ precache-manifest.bcac772474c295e1396941f6f23e16a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce82027f7b4816526588",
+    "url": "/static/css/main.8939657c.chunk.css"
+  },
+  {
+    "revision": "ce82027f7b4816526588",
+    "url": "/static/js/main.ce82027f.chunk.js"
+  },
+  {
+    "revision": "91b7bb1751ce021a4d73",
+    "url": "/static/js/1.91b7bb17.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b28dece12e926899051806c985c93f3",
+    "url": "/index.html"
+  }
+];

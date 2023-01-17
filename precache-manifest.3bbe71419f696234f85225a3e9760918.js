5beed6b634b9d6c6bc2606cef46b8b53bfea5ec7
GreenCheeseWheel <thegreencheesewheel@gmail.com>
2023-01-17 16:18:25 -0300

    Updates

---
+++ precache-manifest.3bbe71419f696234f85225a3e9760918.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "06152b9ff6ee5270507f",
+    "url": "/static/css/main.46762932.chunk.css"
+  },
+  {
+    "revision": "06152b9ff6ee5270507f",
+    "url": "/static/js/main.06152b9f.chunk.js"
+  },
+  {
+    "revision": "25152894afd9e30615a7",
+    "url": "/static/js/1.25152894.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3e08c827dfae895b9c881f780abc60dd",
+    "url": "/static/media/CV_CostaLucas.3e08c827.pdf"
+  },
+  {
+    "revision": "3ba4464747cde1c115faf70967c6746e",
+    "url": "/static/media/github.3ba44647.svg"
+  },
+  {
+    "revision": "17d4d4292efcde2661d79cbe40c6b3c3",
+    "url": "/static/media/linkedin.17d4d429.svg"
+  },
+  {
+    "revision": "398bd7bb0ba810e1592d0b248067f7a9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56f266dadca2a2a30cde04cd8a774d2b.js"
+  "/precache-manifest.3bbe71419f696234f85225a3e9760918.js"
 );
 
 workbox.clientsClaim();
